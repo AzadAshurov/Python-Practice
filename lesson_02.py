47981100996ba84faf92f5cf8b17e06005185636
Azad Ashurov <wertdrof@gmail.com>
2026-08-15 21:24:23 +0400

    Types and classes

--- lesson_02.py
+++ lesson_02.py
@@ -1,1 +1,18 @@
 print("Hello World")
+from dataclasses import dataclass, field
+import uuid
+@dataclass
+class Student:
+    name: str
+    age: int
+    gpa: float
+    id: str = field(default_factory=uuid.uuid4)
+
+student_1 = Student("Alice", 20, 3.8)
+student_2 = Student("Bob", 22, 3.5)
+student_3 = Student("Alice", 20, 3.8)
+
+print(student_1)
+print(student_1.id)
+print(student_1 == student_2 )
+print(student_1 == student_3 )
