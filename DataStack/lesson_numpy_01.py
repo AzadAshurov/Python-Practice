2326508025a3e0fcb62ed28cebb3ff4d0986f79a
Azad Ashurov <wertdrof@gmail.com>
2026-08-25 22:45:11 +0400

    Numpy started

---
+++ DataStack/lesson_numpy_01.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print(numbers)
+print(type(numbers))
+print(numbers.shape)
+new_numbers = numbers * 3
+matrix = np.array([
+ [1,2,3],
+ [4,5,6]
+])
+
+print(matrix[0]) 
+print(matrix[0, 1])
