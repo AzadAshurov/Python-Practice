52889b965c43e74a94b1f05a39b20735ab03073d
Azad Ashurov <wertdrof@gmail.com>
2026-08-25 23:26:51 +0400

    Numpy array types

--- DataStack/lesson_numpy_01.py
+++ DataStack/lesson_numpy_01.py
@@ -13,3 +13,16 @@ matrix = np.array([
 
 print(matrix[0]) 
 print(matrix[0, 1])
+
+print(numbers.dtype)
+a = np.array([1, 2, 3])
+b = np.array([1.5, 2.5, 3.5])
+c = np.array([True, False, True])
+print(a.dtype)
+print(b.dtype)
+
+print(c.dtype)
+print(c)
+d = np.array([1, 2, 3.5])
+print(d.dtype)
+print(d)
